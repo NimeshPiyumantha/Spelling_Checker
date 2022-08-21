7302415f6fdd3a1357f0f6583aff64120f137195
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2022-08-21 23:47:23 +0530

    Add Text Input Filed

--- main.py
+++ main.py
@@ -10,4 +10,8 @@ root.config(background="#dae6f6")
 heading= Label(root,text="Spelling Checker",font=("Trebuchet MS",30,"bold"),bg="#dae6f6",fg="#364971")
 heading.pack(pady=(50,0))
 
+enter_text=Entry(root,justify="center",width=30,font=("poppins",25),bg="white",border=2)
+enter_text.pack(pady=10)
+enter_text.focus()
+
 root.mainloop()
